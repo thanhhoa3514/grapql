51e1b6d6f104dad232d8803f5a0b627a3fe87fb9
thanhhhoa3514 <ngothanhhoa3514@gmail.com>
2024-12-06 09:40:53 +0700

    New article

--- resolvers.ts
+++ resolvers.ts
@@ -1,14 +1,43 @@
 import Article from "./models/article.model";
-
+import { IArticle } from "./typeDefs";
 // 
 export const resolvers =  {
     Query: {
         hello: () => "Hello, world!",
-        getListArticle:async ()=>{
-            const articles = await Article.find({
-                deleted: false
-            })
-            return articles;
+        getListArticle: async (): Promise<IArticle[]> => {
+            const articles = await Article.find({ deleted: false }).lean();
+            return articles.map(article => ({
+                ...article,
+                _id: article._id.toString()
+            }));
+        },
+        getArticleById: async (_: any, { id }: { id: string }): Promise<IArticle | null> => {
+            const article = await Article.findById(id);
+            return article ? {
+                ...article.toObject(),
+                _id: article._id.toString()
+            } : null;
         }
     },
+    Mutation: {
+        addArticle: async (_: any, { input }: { input: IArticle }): Promise<IArticle> => {
+            const article = new Article(input);
+            await article.save();
+            return {
+                _id: article._id.toString(),
+               ...input
+            };
+        },
+        updateArticle: async (_: any, { id, input }: { id: string, input: Partial<IArticle> }): Promise<IArticle | null> => {
+            const article = await Article.findByIdAndUpdate(id, input, { new: true });
+            return article? {
+                _id: article._id.toString(),
+               ...input
+            } : null;
+        },
+        deleteArticle: async (_: any, { id }: { id: string }): Promise<boolean> => {
+            await Article.findByIdAndUpdate(id, { deleted: true });
+            return true;
+        }
+    }
 };
--- typeDefs.ts
+++ typeDefs.ts
@@ -1,10 +1,17 @@
 import { gql } from "apollo-server-express";
+export interface IArticle {
+    _id?: string,
+    title?: string,
+    avatar?: string,
+    description?: string,
+    deleted?: boolean;
 
+}
 // Define the GraphQL schema
 export const typeDefs = gql`
 
     type Article{
-        id:ID,
+        id:ID!,
         title: String,
         avatar: String,
         description: String,
@@ -14,5 +21,17 @@ export const typeDefs = gql`
     type Query {
         hello: String,
         getListArticle:[Article]
+        getArticleById(id:ID):Article
     }
+    input ArticleInput{
+        title:String!,
+        avatar:String,
+        description:String!
+    }
+    type Mutation {
+        addArticle(article:ArticleInput): Article
+        updateArticle(id:ID!, title:String!, avatar:String, description:String!): Article
+        deleteArticle(id:ID!): Boolean
+    }
+ 
 `;
